6896a103b3b5bf363e34288b22dc3744ef61c88a
lemus2022 <119651221+lemus2022@users.noreply.github.com>
2023-03-21 16:15:28 -0600

    Add files via upload

--- src/models/movies.ts
+++ src/models/movies.ts
@@ -1,4 +1,11 @@
-export interface Movie {
-    title: string;
-    yesr: number;
-}
+import express , {Request, Response} from 'express';
+import controller from '../controllers/movies';
+const router = express.Router();
+
+.get('/movies', controller.getAll)
+.get('/movies/:id', controller.getAll)
+.post('/movies', controller.getAll)
+.patch('/movies', controller.getAll)
+.delete('/movies', controller.getAll)
+
+export = router;
